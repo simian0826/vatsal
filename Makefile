2d7f4d53bd6e97ca6819d40d2a0b6f340176eeef
brunli <brunli@deloitte.com.cn>
2023-09-25 19:50:15 +0800

    build: modify docker file

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 stop:
 	@docker stop vatsal:1.0
 build:
-	@docker build -t vatsal:1.0
+	@docker build . -t vatsal:1.0
 run:
 	@docker run -d -p 80:80 vatsal:1.0
--- dist/assets/index-ebb19efb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,e as c,o as n}from"./index-23083582.js";const o={};function r(t,s){return n(),c("div",null,"404")}const a=e(o,[["render",r]]);export{a as default};
